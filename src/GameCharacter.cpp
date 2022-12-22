ecaa3a3467affd78e932908e390153abbe5adfb4
LapoBernacchioni <LapoBernacchioni@github.com>
2022-12-22 10:06:02 +0100

    GameCharacter initialization fix

--- src/GameCharacter.cpp
+++ src/GameCharacter.cpp
@@ -17,12 +17,11 @@ void GameCharacter::updateStepQueue (Map &map,const sf::Vector2<float> target){
 }
 
 //PLAYER
-BaseStats Player::baseStats={100,100,0};
 
 Player::Player  (uint8_t health, uint8_t energy, bool filterColorR, bool filterColorG, bool filterColorB):
-                GameCharacter(  baseStats.maxHealth,(health>baseStats.maxHealth)?baseStats.maxHealth:health,
-                                baseStats.maxEnergy,(energy>baseStats.maxEnergy)?baseStats.maxEnergy:energy,
-                                baseStats.priority){
+                GameCharacter(  100,(health>100)?100:health,
+                                100,(energy>100)?100:energy,
+                                0){
 
 	static sf::Texture* texture;
 	if (!texture)
@@ -45,12 +44,11 @@ Player::Player  (uint8_t health, uint8_t energy, bool filterColorR, bool filterC
 }
 
 //MELEE
-BaseStats Melee::baseStats={30,10,2};
 
 Melee::Melee  (uint8_t health, uint8_t energy):
-                GameCharacter(  baseStats.maxHealth,(health>baseStats.maxHealth)?baseStats.maxHealth:health,
-                                baseStats.maxEnergy,(energy>baseStats.maxEnergy)?baseStats.maxEnergy:energy,
-                                baseStats.priority){
+                GameCharacter(  30,(health>30)?30:health,
+                                10,(energy>10)?10:energy,
+                                2){
 
 	static sf::Texture* texture;
 	if (!texture)
@@ -69,12 +67,11 @@ Melee::Melee  (uint8_t health, uint8_t energy):
     movementStrategy = std::unique_ptr<PathAlgorithm>(new AStar());
 }
 //BAT
-BaseStats Bat::baseStats={10,30,3};
 
 Bat::Bat  (uint8_t health, uint8_t energy):
-                GameCharacter(  baseStats.maxHealth,(health>baseStats.maxHealth)?baseStats.maxHealth:health,
-                                baseStats.maxEnergy,(energy>baseStats.maxEnergy)?baseStats.maxEnergy:energy,
-                                baseStats.priority){
+                GameCharacter(  10,(health>10)?10:health,
+                                30,(energy>30)?30:energy,
+                                3){
 
 	static sf::Texture* texture;
 	if (!texture)
@@ -94,12 +91,11 @@ Bat::Bat  (uint8_t health, uint8_t energy):
 }
 //RANGED
 
-BaseStats Ranged::baseStats={5,50,1};
 
 Ranged::Ranged  (uint8_t health, uint8_t energy):
-                GameCharacter(  baseStats.maxHealth,(health>baseStats.maxHealth)?baseStats.maxHealth:health,
-                                baseStats.maxEnergy,(energy>baseStats.maxEnergy)?baseStats.maxEnergy:energy,
-                                baseStats.priority), animationDuration(1), animationTime(0){
+                GameCharacter(  5,(health>5)?5:health,
+                                50,(energy>50)?50:energy,
+                                1), animationDuration(1), animationTime(0){
 
 	static sf::Texture* texture;
 	if (!texture)
