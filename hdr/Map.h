525106a9620b3731c7b3f933a697c305757fa162
RoccoTescaro <rocco.tescaro@stud.unifi.it>
2023-02-01 11:34:42 +0100

    Updated map

--- hdr/Map.h
+++ hdr/Map.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <SFML/System.hpp>
+#include "Serializable.h"
+#include "Archive.h"
 #include <map>
 #include <memory>
 
 //#TODO remove
-class Tile; 
+class Entity;
+class Tile;
 class GameCharacter;
 class Player;
 
-class Map
+class Map //: public Serializable
 {
 	struct Compare
 	{
@@ -22,38 +25,41 @@ class Map
 	using GameCharacters = std::map<sf::Vector2<int>, std::shared_ptr<GameCharacter>, Compare>;
 
 public:
-
 	void render();
 
 	inline Tiles& getTiles() { return tiles; };
 	inline const Tiles& getTiles() const { return tiles; };
 	inline GameCharacters& getGameCharacters() { return gameCharacters; };
 	inline const GameCharacters& getGameCharacters() const { return gameCharacters; };
- 
-	std::shared_ptr<Tile> getTile(const sf::Vector2<int>& pos); //{ return *tiles.at(pos); };
-	std::shared_ptr<GameCharacter> getGameCharacter(const sf::Vector2<int>& pos) ;
+
+	std::shared_ptr<Tile> getTile(const sf::Vector2<int>& pos); //Cannot return a reference in case there is no entity at given pos
+	std::shared_ptr<GameCharacter> getGameCharacter(const sf::Vector2<int>& pos);
 	std::shared_ptr<Player> getPlayer();
 
 	inline const sf::Vector2<int>& getCellDim() const { return cellDim; };
 
-	Map& addTile(Tile* tile);
-	Map& addGameCharacter(GameCharacter* gameCharacter);
+	Map& add(const std::shared_ptr<Entity>& entity);
+	Map& addTile(const std::shared_ptr<Tile>& tile);
+	Map& addGameCharacter(const std::shared_ptr<GameCharacter>& gameCharacter);
 
+	Map& remove(const sf::Vector2<int>& pos);
 	Map& removeTile(const sf::Vector2<int>& pos);
 	Map& removeGameCharacter(const sf::Vector2<int>& pos);
 
 	void move(const sf::Vector2<int>& start, const sf::Vector2<int>& end);
 
-	//inline bool isOccupied(const sf::Vector2<int>& pos);
+	bool isOccupied(const sf::Vector2<int>& pos, bool solid);
 
-	inline sf::Vector2<float> posIntToFloat();
-	inline sf::Vector2<int> posFloatToInt(); //convert world coord to grided one
+	inline sf::Vector2<float> posIntToFloat(const sf::Vector2<int>& pos);
+	inline sf::Vector2<int> posFloatToInt(const sf::Vector2<float>& pos); //convert world coord to grided one
 
+	//void serialize(Archive& fs) override; //#TODO fix sf::Vector2 save
 private:
+	//REGIST(Map);
 
 	Tiles tiles;
 	GameCharacters gameCharacters;
-	std::shared_ptr<Player> player; 
+	std::shared_ptr<Player> player;
 
 	sf::Vector2<int> cellDim{ 32,32 };
 };
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,28 +1,144 @@
 #include "../hdr/Map.h"
+#include "../hdr/Entity.h"
+#include "../hdr/Tile.h"
+#include "../hdr/GameCharacter.h"
 
 void Map::render()
 {
 	for (auto& tile : tiles)
-		break;//tile.second->render(); 
+		tile.second->render();
 	for (auto& gameCharacter : gameCharacters)
-		break;//gameCharacter.second->render();
+		gameCharacter.second->render();
 };
 
-std::shared_ptr<Tile> Map::getTile(const sf::Vector2<int>& pos) 
+std::shared_ptr<Tile> Map::getTile(const sf::Vector2<int>&pos)
 {
 	if (tiles.find(pos) != tiles.end())
 		return tiles.at(pos);
 	return nullptr;
 }
 
-std::shared_ptr<GameCharacter> Map::getGameCharacter(const sf::Vector2<int>& pos) 
+std::shared_ptr<GameCharacter> Map::getGameCharacter(const sf::Vector2<int>&pos)
 {
 	if (gameCharacters.find(pos) != gameCharacters.end())
 		return gameCharacters.at(pos);
 	return nullptr;
 }
 
-std::shared_ptr<Player> Map::getPlayer() 
+std::shared_ptr<Player> Map::getPlayer()
 {
 	return player;
 }
+
+Map& Map::add(const std::shared_ptr<Entity>&entity)
+{
+	std::shared_ptr<Tile> ptrTile{ dynamic_cast<Tile*>(entity.get()) };
+	std::shared_ptr<GameCharacter> ptrGameCharacter{ dynamic_cast<GameCharacter*>(entity.get()) };
+	if (ptrTile) addTile(ptrTile);
+	else if (ptrGameCharacter) addGameCharacter(ptrGameCharacter);
+	else ERROR("no suitable conversion for entity");
+	return *this;
+}
+
+Map& Map::addTile(const std::shared_ptr<Tile>&tile)
+{
+	sf::Vector2<int> pos = posFloatToInt(tile->getCenter());
+	if (tiles.count(pos))
+		removeTile(pos);
+	tiles[pos] = static_cast<std::shared_ptr<Tile>>(tile);
+	return *this;
+}
+
+Map& Map::addGameCharacter(const std::shared_ptr<GameCharacter>&gameCharacter)
+{
+	sf::Vector2<int> pos = posFloatToInt(gameCharacter->getCenter());
+	if (gameCharacters.at(pos))
+		removeGameCharacter(pos);
+
+	gameCharacters[pos] = static_cast<std::shared_ptr<GameCharacter>>(gameCharacter);
+	Player* ptrPlayer = dynamic_cast<Player*>(gameCharacter.get());
+	if (ptrPlayer)
+		player = static_cast<std::shared_ptr<Player>>(ptrPlayer);
+	return *this;
+}
+
+Map& Map::remove(const sf::Vector2<int>&pos)
+{
+	removeTile(pos);
+	removeGameCharacter(pos);
+	return *this;
+}
+
+Map& Map::removeTile(const sf::Vector2<int>&pos)
+{
+	tiles.erase(pos);
+	return *this;
+}
+
+Map& Map::removeGameCharacter(const sf::Vector2<int>&pos)
+{
+	gameCharacters.erase(pos);
+	return *this;
+}
+
+void Map::move(const sf::Vector2<int>&start, const sf::Vector2<int>&end)
+{
+	auto gameCharacter = gameCharacters.extract(start);
+	if (gameCharacter) gameCharacters[end] = gameCharacter.mapped();
+	else ERROR("no gameCharacter at start position");
+}
+
+bool Map::isOccupied(const sf::Vector2<int>&pos, bool solid)
+{
+	return getTile(pos) && (getTile(pos).get()->isSolid() || solid);
+}
+
+inline sf::Vector2<float> Map::posIntToFloat(const sf::Vector2<int>&pos)
+{
+	return sf::Vector2<float>(pos.x * cellDim.x, pos.y * cellDim.y);
+}
+
+inline sf::Vector2<int> Map::posFloatToInt(const sf::Vector2<float>&pos)
+{
+	return sf::Vector2<int>(std::floor(pos.x / cellDim.x), std::floor(pos.y / cellDim.y));
+}
+
+/*void Map::serialize(Archive& fs)
+{
+	//can't easily serialize the tiles and gamecharacters maps becoude of compare function
+	//but we can save each entity one by one
+	switch (fs.getMode())
+	{
+	case Archive::Save:
+	{
+		uint32_t sizeTiles = tiles.size();
+		fs.serialize(sizeTiles);
+		for (auto& tile : tiles)
+			fs.serialize(*tile.second.get());
+		uint32_t sizeGameCharacters = gameCharacters.size();
+		fs.serialize(sizeGameCharacters);
+		for (auto& gameCharacter : gameCharacters)
+			fs.serialize(gameCharacter.second);
+	}
+	case Archive::Load:
+	{
+		uint32_t sizeTiles;
+		fs.serialize(sizeTiles);
+		for (uint32_t i = 0; i < sizeTiles; i++)
+		{
+			std::shared_ptr<Tile> tile;
+			fs.serialize(tile);
+			addTile(tile);
+		}
+		uint32_t sizeGameCharacters;
+		fs.serialize(sizeGameCharacters);
+		for (uint32_t i = 0; i < sizeGameCharacters; i++)
+		{
+			std::shared_ptr<GameCharacter> gameCharacter;
+			fs.serialize(gameCharacter);
+			addGameCharacter(gameCharacter);
+		}
+	}
+	}
+
+}*/
